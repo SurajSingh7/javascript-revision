aab9473ef8c0ef786a39fba52a16d02f6c1903a9
SurajSingh7 <220suraj@gmail.com>
2023-11-21 23:37:09 +0530

    asynCheck

---
+++ asynCheck1.js
@@ -0,0 +1,16 @@
+// node asynCheck1
+
+console.log("start");
+
+async function suraj(){
+    console.log("middle1");
+}
+
+suraj();
+console.log("end");
+
+
+// PS D:\@mern\javascript-revision> node asynCheck1
+// start
+// middle1
+// end
---
+++ asynCheck2.js
@@ -0,0 +1,20 @@
+// node asynCheck2
+
+console.log("start");
+
+async function suraj(){
+    console.log("middle1");
+    return "middle2"
+}
+
+var x=suraj();
+console.log(x);
+console.log("end");
+
+
+
+// PS D:\@mern\javascript-revision> node asynCheck2
+// start
+// middle1
+// Promise { 'middle2' }
+// end
