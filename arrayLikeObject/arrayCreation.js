939b58a03c5e64c65b792a7c3d43ae23b7164c0a
SurajSingh7 <220suraj@gmail.com>
2023-11-26 23:48:57 +0530

    .

---
+++ arrayLikeObject/arrayCreation.js
@@ -0,0 +1,19 @@
+// node arrayCreation
+
+var a=new Array(3,1); // array value-> [3,1]  & 2 lengh
+console.log(a);
+
+var b=new Array(3);  // length -> 3
+console.log(b);
+
+var c=new Array([1,2,3]);  // like -> 2d array
+console.log(c);
+
+// OUTPUT
+// PS D:\@mern\javascript-revision\arrayLikeObject> node arrayCreation
+// [ 3, 1 ]
+// [ <3 empty items> ]
+// [ [ 1, 2, 3 ] ]
+
+
+
